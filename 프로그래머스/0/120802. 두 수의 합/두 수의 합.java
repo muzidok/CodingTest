986ebcc79558a40a2038f4c9801c3b13889023a9
muzidok <156971991+muzidok@users.noreply.github.com>
2024-02-03 16:29:10 +0900

    [level 0] Title: 두 수의 합, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        if(num1 <= 50000 && num1 >= -50000 && num2 <= 50000 && num2 >= -50000){
+           return num1 + num2;
+        }
+        return -1;
+    }
+}
