bd7d41608ad0420c68ef1b448077d9daf7542216
muzidok <156971991+muzidok@users.noreply.github.com>
2024-02-23 21:02:24 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    const newArr = numbers.sort((a,b) => a - b);
+    for(let i = 0; i < newArr.length; i++ ){
+       
+        return newArr[newArr.length-1] * newArr[newArr.length-2];
+    }
+            
+        
+}
